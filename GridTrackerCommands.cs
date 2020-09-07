f86bf769ccef4c428c0ce5e51bf15b4df52b3236
Emanuele Disco <emawind84@gmail.com>
2020-09-07 17:10:17 +0900

    update

---
+++ GridTrackerCommands.cs
@@ -0,0 +1,115 @@
+﻿using ALE_Core.Utils;
+using NLog;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Screens.Helpers;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch.Commands;
+using Torch.Commands.Permissions;
+using VRage.Game.ModAPI;
+using VRage.Groups;
+using VRageMath;
+
+namespace GridTrackerPlugin
+{
+    public class GridTrackerCommands : CommandModule
+    {
+
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public GridTrackerPlugin Plugin => (GridTrackerPlugin)Context.Plugin;
+
+        [Command("trackgrid", "Track the grid you are looking at, by adding a GPS to its location")]
+        [Permission(MyPromoteLevel.SpaceMaster)]
+        public void Track()
+        {
+            IMyPlayer player = Context.Player;
+
+            if (player == null)
+            {
+
+                Context.Respond("Console has no Character so cannot use this command. Use !protect <gridname> instead!");
+                return;
+            }
+
+            IMyCharacter character = player.Character;
+            if (character == null)
+            {
+                Context.Respond("You have no Character currently. Make sure to spawn and be out of cockpit!");
+                return;
+            }
+
+            try
+            {
+                var groups = GridFinder.FindLookAtGridGroupMechanical(Context.Player?.Character);
+
+                if (!CheckGroups(groups, out MyGroups<MyCubeGrid, MyGridMechanicalGroupData>.Group group, Context))
+                    return;
+
+                foreach (MyGroups<MyCubeGrid, MyGridMechanicalGroupData>.Node groupNodes in group.Nodes)
+                {
+                    MyCubeGrid grid = groupNodes.NodeData;
+
+                    MyGps gps = new MyGps
+                    {
+                        Coords = grid.PositionComp.GetPosition(),
+                        Name = string.Format("{0}", grid.DisplayName),
+                        ShowOnHud = true,
+                        GPSColor = Color.Orange,
+                        DiscardAt = new TimeSpan?()
+                    };
+                    gps.SetEntity(grid);
+                    gps.UpdateHash();
+
+                    Context.Torch.CurrentSession.KeenSession.Gpss.SendAddGps(player.Identity.IdentityId, ref gps, grid.EntityId, false);
+                }
+
+                Context.Respond("Grid is getting tracked!");
+
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Error trying to track the grid");
+            }
+
+        }
+
+        public static bool CheckGroups(ConcurrentBag<MyGroups<MyCubeGrid, MyGridMechanicalGroupData>.Group> groups,
+                out MyGroups<MyCubeGrid, MyGridMechanicalGroupData>.Group group, CommandContext Context)
+        {
+
+            /* No group or too many groups found */
+            if (groups.Count < 1)
+            {
+
+                Context.Respond("Could not find the Grid.");
+                group = null;
+
+                return false;
+            }
+
+            /* too many groups found */
+            if (groups.Count > 1)
+            {
+
+                Context.Respond("Found multiple Grids with same Name. Make sure the name is unique.");
+                group = null;
+
+                return false;
+            }
+
+            if (!groups.TryPeek(out group))
+            {
+                Context.Respond("Could not work with found grid for unknown reason.");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ GridTrackerPlugin.cs
@@ -0,0 +1,77 @@
+﻿using NLog;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Screens.Helpers;
+using Sandbox.Game.World;
+using Sandbox.ModAPI;
+using SpaceEngineers.Game.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch;
+using Torch.API;
+using Torch.API.Managers;
+using Torch.API.Plugins;
+using Torch.API.Session;
+using Torch.Commands;
+using Torch.Managers.ChatManager;
+using Torch.Session;
+using VRage.Game.Entity;
+using VRage.ModAPI;
+using VRageMath;
+
+namespace GridTrackerPlugin
+{
+    public class GridTrackerPlugin : TorchPluginBase
+    {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        private readonly Stopwatch stopWatch = new Stopwatch();
+
+        /// <inheritdoc />
+        public override void Init(ITorchBase torch)
+        {
+            base.Init(torch);
+
+            var sessionManager = Torch.Managers.GetManager<TorchSessionManager>();
+            if (sessionManager != null)
+                sessionManager.SessionStateChanged += SessionChanged;
+            else
+                Log.Warn("No session manager loaded!");
+        }
+
+        private void SessionChanged(ITorchSession session, TorchSessionState newState)
+        {
+
+            if (newState == TorchSessionState.Loaded)
+            {
+                stopWatch.Start();
+                Log.Info("Session loaded, start backup timer!");
+            }
+            else if (newState == TorchSessionState.Unloading)
+            {
+                stopWatch.Stop();
+                Log.Info("Session Unloading, suspend backup timer!");
+            }
+        }
+
+        public static long GetOwner(MyCubeGrid grid)
+        {
+
+            var gridOwnerList = grid.BigOwners;
+            var ownerCnt = gridOwnerList.Count;
+            var gridOwner = 0L;
+
+            if (ownerCnt > 0 && gridOwnerList[0] != 0)
+                return gridOwnerList[0];
+            else if (ownerCnt > 1)
+                return gridOwnerList[1];
+
+            return gridOwner;
+        }
+    }
+}
